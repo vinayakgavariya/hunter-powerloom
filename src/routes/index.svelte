0ed9f089709efcca8c12bfeebda486b6347b3a12
Swaroop Hegde <email@swaroophegde.com>
2022-06-24 20:01:13 -0700

    added support for sync status + proof and IPFS links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,7 @@
   import { LinkedChart, LinkedLabel, LinkedValue } from "svelte-tiny-linked-charts";
   import axios from "axios";
   import { onMount } from 'svelte';
+  import Time from "svelte-time";
 
   let data = {
     "2005-01-01": 25,
@@ -235,19 +236,40 @@
 
   </dl>
 </div>
-<div class="py-4 flex justify-between">
-  <h3 class="text-lg font-medium text-gray-900">
-    Top Pairs
-  </h3>
-  {#if pairsData.block_height}Synced to <a href="https://etherscan.io/block/{pairsData.block_height}"class="text-indigo-800" target="_blank">{pairsData.block_height}</a>
-  {/if} {pairsData.block_timestamp}
-  {#if pairsData.cid}
-  <a href="https://ipfs.io/ipfs/{pairsData.cid}"class="text-indigo-800" target="_blank">IPFS</a>
-  {/if}
-  {#if pairsData.txHash}
-  <a href="https://explorer-prost.powerloom.io/tx/{pairsData.txHash}"class="text-indigo-800" target="_blank">Proof</a>
-  {/if}
-  <a href="/pairs"class="text-indigo-800">See all</a>
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="pt-4">
+<div class="bg-white px-4 py-5 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg sm:px-6 ">
+  <div class="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap">
+    <div class="ml-4 mt-4">
+      <div class="flex items-center">
+        <div class="flex-shrink-0">
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+        </div>
+        <div class="ml-4">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">Top Pairs</h3>
+          <p class="text-sm text-gray-500">
+            {#if pairsData.block_height}Synced to <a href="https://etherscan.io/block/{pairsData.block_height}"class="text-indigo-800" target="_blank">{pairsData.block_height}</a> <Time relative timestamp={pairsData.block_timestamp} />{/if}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="ml-4 mt-4 flex-shrink-0 flex">
+      <a class="relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" target="_blank" href="https://cloudflare-ipfs.com/ipfs/{pairsData.cid}">
+        <!-- Heroicon name: solid/phone -->
+        <svg class="-ml-1 mr-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z" />
+        </svg>
+        <span> IPFS </span>
+      </a>
+      <a class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" target="_blank" href="https://explorer-prost.powerloom.io/tx/{pairsData.txHash}">
+        <!-- Heroicon name: solid/mail -->
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
+        <span> Proof </span>
+      </a>
+    </div>
+  </div>
+</div>
 </div>
 <div class="flex flex-col">
   <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -314,13 +336,17 @@
             </tr>
             {/each}
           </tbody>
-        </table>
+      </table>
+      <div class="">
+        <a href="/pairs" class="w-full flex justify-center items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"> View all </a>
       </div>
+</div>
     </div>
   </div>
 </div>
 <div class="py-4 flex justify-between">
   <h3 class="text-lg font-medium text-gray-900">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right: 0.75rem;"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="3"></circle></svg>
     Top Tokens
   </h3>
   <a href="/tokens"class="text-indigo-800">See all</a>
