88b64103ba78917c7becdf517e8b5f49911c6dc4
Swaroop Hegde <email@swaroophegde.com>
2022-07-06 21:54:18 -0700

    fixed pairs link on mobile - need to fix menu

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -114,8 +114,8 @@
           Overview
         </a>
 
-        <a href="/pools" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
-          Pools
+        <a href="/pairs" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
+          Pairs
         </a>
 
         <a href="/tokens" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
