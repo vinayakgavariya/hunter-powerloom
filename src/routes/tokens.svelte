a447f58d12feb191a77d056ccc3f4c308b628744
Swaroop Hegde <email@swaroophegde.com>
2022-07-14 22:38:40 -0700

    change all explorer links

--- src/routes/tokens.svelte
+++ src/routes/tokens.svelte
@@ -93,7 +93,7 @@
           </svg>
           <span> IPFS </span>
         </a>
-        <a class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" target="_blank" href="https://explorer-prost.powerloom.io/tx/{tokenData.txHash}">
+        <a class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" target="_blank" href="https://polygonscan.com/tx/{tokenData.txHash}#eventlog">
           <!-- Heroicon name: solid/mail -->
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
