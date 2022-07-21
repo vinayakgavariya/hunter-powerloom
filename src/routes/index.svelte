85ca5702e0f3317526dfd9c38cf18c89ef8e3cc9
Swaroop Hegde <email@swaroophegde.com>
2022-07-20 17:09:52 -0700

    add stats notice of current pairs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -121,6 +121,22 @@
 {/if}
 
 {#if statsData }
+<div class="rounded-md bg-blue-50 p-4">
+  <div class="flex">
+    <div class="flex-shrink-0">
+      <!-- Heroicon name: solid/information-circle -->
+      <svg class="h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+      </svg>
+    </div>
+    <div class="ml-3 flex-1 md:flex md:justify-between">
+      <p class="text-sm text-blue-700">We current show stats for a specific list of pairs/pools with high liquidity. Dynamic support for other pairs will be added soon!</p>
+      <p class="mt-3 text-sm md:mt-0 md:ml-6">
+        <a href="/pairs" class="whitespace-nowrap font-medium text-blue-700 hover:text-blue-600">Current Pairs <span aria-hidden="true">&rarr;</span></a>
+      </p>
+  </div>
+  </div>
+</div>
 <div>
   <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
     <div class="relative bg-white pt-5 px-4 pb-3 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
